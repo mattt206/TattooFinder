6d1555fb2a98bc550959e368a6d86dd1550096b2
mateo holguin <mattt206@gmail.com>
2022-10-24 16:59:20 -0500

    controller user and profile working, log-in fixed too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   def profile
-    @user = User.find(params[:id])
-    @artist = params[:artist]
-    @resultuser = User.where("id = ? ", @artist.to_i)
+    if @user.nil?
+      if User.find(params[:id]) != nil
+        @user = User.find(params[:id])
+        @artist = params[:artist]
+        @resultuser = User.where("id = ? ", @artist.to_i)
+      else
+      end
+
+    end
+    
     # redirect_to users_profile_path(@resultuser)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  # get "users/profile/#{User.id}"
-  # get "#{User.id}/profile"
-  # get :users, to: "user#profile"
-  # get "users/profile/#{:id}", to: 'users#profile'
-  get '/users/:id', to: 'users#profile', as: 'profile'
   devise_for :users do
     resources :providers, only: %i[new create edit update destroy]
   end
-  # resources :users, only: %i[profile]
+
+  get '/users/:id', to: 'users#profile', as: 'profile'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
