a7d46275d94d42e0c5c0b8aeeecd40209ed1aad8
mateo holguin <mattt206@gmail.com>
2022-10-25 03:22:06 -0500

    bookings rady

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,48 @@
 class BookingsController < ApplicationController
+  before_action :set_bookings, only: %i[show edit update destroy]
+
+
+  def new
+    @user = User.find(params[:user_id])
+    @booking = Booking.new
+  end
+
+  def show
+    @artist = User.find(@booking.artist_info)
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @booking = Booking.new(bookings_params)
+    @booking.user_id = current_user.id
+    @booking.status = false
+    @booking.artist_info = params["booking"][:artist]
+    if @booking.save
+      redirect_to  booking_path(@booking.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def edit
+  end
+
+  def destroy
+    @user.id = @booking.user_id
+    @booking.destroy
+    redirect_to profile_path(@user.id), status: :see_other
+  end
+
+  private
+
+  def set_bookings
+    @booking = Booking.find(params[:id])
+  end
+
+  def bookings_params
+    params.require(:booking).permit(:start_time, :end_time, :artist_info)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,18 @@ Rails.application.routes.draw do
   end
 
   get '/users/:id', to: 'users#profile', as: 'profile'
+  # get '/users/:id/bookings/new', to: 'bookings#new', as: 'booking'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :services do
-    resources :bookings, only: %i[new create]
+    # resources :bookings, only: %i[new create]
     resources :reviews, only: %i[new create]
+  end
+  resources :users do
+    resources :bookings, only: %i[new create]
   end
   resources :providers
   resources :bookings, except: %i[new create]
