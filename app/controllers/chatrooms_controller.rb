ea7fcf2c033e430d41607beb4b82bb5752b06ed3
mateo holguin <mattt206@gmail.com>
2022-11-03 17:55:00 -0500

    chat ready, new migration, chat in profile artist

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_artist, only: %i[create show]
   # crear el chat
   # capturar el current_user y el id del profile
   # enlazar estos dos y mostrar
@@ -13,24 +14,31 @@ class ChatroomsController < ApplicationController
   def create
     # @current_artist = User.find(params[:user_id])
     # @user = current_user
-    @artist = User.find(params[:user_id])
-    params[:newkey] = @artist.id
     @chatroom = Chatroom.new(chats_params)
     if @chatroom.save
-      redirect_to chatroom_path(@chatroom.id, @artist)
+      # redirect_to chatroom_path(@chatroom.id)
+      # redirect_to chatroom_path(@chatroom.id, @artist)
+      redirect_to user_chatroom_path(@artist.id, @chatroom.id)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
+   if !params[:format].nil?
     @artist = User.find(params[:format])
+    params[:newartistkey] = @artist.id
+   end
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
-    params[:newartistkey] = @artist.id
+    @message[:artist_info] = @artist.id
   end
 
   private
+
+  def set_artist
+    @artist = User.find(params[:user_id])
+  end
 
   def chats_params
     params.require(:chatroom).permit(:name)
