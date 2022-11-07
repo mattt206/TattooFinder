7bdae762ae5b6420996cc7f7194359466e48b395
mateo holguin <mattt206@gmail.com>
2022-11-07 18:23:25 -0500

    logic to add new artist ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_03_195909) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_231143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_03_195909) do
     t.string "category", default: [], null: false, array: true
     t.float "latitude"
     t.float "longitude"
+    t.integer "owner"
   end
 
   create_table "reviews", force: :cascade do |t|
